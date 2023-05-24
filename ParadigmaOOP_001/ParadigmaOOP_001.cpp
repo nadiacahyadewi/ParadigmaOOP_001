4477b885cad998cf5e11dfbf2be59ab1b725211d
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-24 15:47:23 +0700

    menambahkan fungsi utama siswa

--- ParadigmaOOP_001/ParadigmaOOP_001.cpp
+++ ParadigmaOOP_001/ParadigmaOOP_001.cpp
@@ -37,4 +37,12 @@ public:
 	}
 };
 
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
+
 
