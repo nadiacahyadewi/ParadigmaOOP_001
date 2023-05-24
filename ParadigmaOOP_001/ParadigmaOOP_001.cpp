574805be18a722e2fa185aa449f863eb521488c9
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-24 15:18:43 +0700

    Add project files.

---
+++ ParadigmaOOP_001/ParadigmaOOP_001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+};
